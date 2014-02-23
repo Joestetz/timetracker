d41c09741e4fa3690ec0cb05c4b6d863ab6b926a
Joestetz <Joestetz@gmail.com>
2014-02-23 15:26:26 -0500

    added duplicate checking when adding labor/absense to enter time page

--- public/js/directives.js
+++ public/js/directives.js
@@ -11,7 +11,10 @@
 				});
 			
 				scope.addAbsense = function() {
-					if(scope.absense != undefined)
+					var matches = jQuery.grep(scope.periodTasks, function(e, i) {
+						return e.task_id == scope.absense._id;
+					});
+					if(scope.absense != undefined && matches.length == 0)
 						scope.periodTasks.push({ 'isAbsense': true, 'task_id': scope.absense._id, 'taskName': scope.absense.name, 'taskDescription': scope.absense.description, 'uid': scope.absense.uid, 'time': [], 'authHours': 0 });
 					element.modal('hide');
 				};
@@ -31,7 +34,10 @@
 				});
 			
 				scope.addLabor = function() {
-					if(scope.labor != undefined)
+					var matches = jQuery.grep(scope.periodTasks, function(e, i) {
+						return e.task_id == scope.labor._id;
+					});
+					if(scope.labor != undefined && matches.length == 0)
 					{
 						scope.periodTasks.push({ 'isAbsense': false, 'task_id': scope.labor._id, 'taskName': scope.labor.name, 'taskDescription': scope.labor.description, 'uid': scope.labor.uid, 'time': [], 'authHours': 0 });
 					}
