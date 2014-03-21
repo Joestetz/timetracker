8361169a8bc4b5ac7df38cc17253ae1bd810abc1
Joestetz <Joestetz@gmail.com>
2014-03-20 22:35:12 -0400

    added myTasksController tests

--- public/test/unit/specs/controllerTests.js
+++ public/test/unit/specs/controllerTests.js
@@ -267,3 +267,155 @@ describe('enterTimeController', function() {
 		});
 	});
 });
+
+describe('myTasksController', function() {
+	var scope, controllerSvc, httpMock;
+	
+	var mockApiResponseUser = {
+		"_id": "52f78c914293916d8c8c281b",
+		"name": "User 2",
+		"taskBank": [
+			{
+				"task_id": "52f78dcf4293916d8c8c281d"
+			},
+			{
+				"task_id": "52f78dcf4293916d8c8c2820"
+			},
+			{
+				"task_id": "52f78dcf4293916d8c8c2821"
+			},
+			{
+				"task_id": "52f78dcf4293916d8c8c2824"
+			}
+		]
+	};
+	
+	var mockApiResponseUserDeletedTask = {
+		"_id": "52f78c914293916d8c8c281b",
+		"name": "User 2",
+		"taskBank": [
+			{
+				"task_id": "52f78dcf4293916d8c8c281d"
+			},
+			{
+				"task_id": "52f78dcf4293916d8c8c2821"
+			},
+			{
+				"task_id": "52f78dcf4293916d8c8c2824"
+			}
+		]
+	};
+		
+	var mockApiResponseTasks = [
+		{
+			"_id": "task0",
+			"name": "Task Name 00",
+			"description": "Description 00",
+			"uid": "B01000000000000000000",
+			"manager_id": "52f78c914293916d8c8c281a"
+		},
+		{
+			"_id": "task3",
+			"name": "Task Name 03",
+			"description": "Description 03",
+			"uid": "B01000000000000000003",
+			"manager_id": "52f78c914293916d8c8c281a"
+		},
+		{
+			"_id": "task4",
+			"name": "Task Name 04",
+			"description": "Description 04",
+			"uid": "B01000000000000000004",
+			"manager_id": "52f78c914293916d8c8c281a"
+		},
+		{
+			"_id": "task7",
+			"name": "Task Name 07",
+			"description": "Description 07",
+			"uid": "B01000000000000000007",
+			"manager_id": "52f78c914293916d8c8c281a"
+		}
+	];
+
+	beforeEach(module('timeTracker'));
+	beforeEach(inject(function($rootScope, $controller, $httpBackend) {
+		scope = $rootScope.$new();
+		controllerSvc = $controller;
+		httpMock = $httpBackend;
+	}));
+	
+	describe('on load', function(){
+		beforeEach(inject(function() {
+			httpMock.whenGET('/api/users/52f78c914293916d8c8c281b').respond(mockApiResponseUser)
+			httpMock.whenGET('/api/tasks?ids=52f78dcf4293916d8c8c281d&ids=52f78dcf4293916d8c8c2820&ids=52f78dcf4293916d8c8c2821&ids=52f78dcf4293916d8c8c2824').respond(mockApiResponseTasks)
+			ctrl = controllerSvc('myTasksController', {$scope: scope});
+			httpMock.flush();
+		}));
+		
+		it('should get taskBank for user from server', function() {
+			httpMock.expectGET('/api/users/52f78c914293916d8c8c281b');
+			httpMock.expectGET('/api/tasks?ids=52f78dcf4293916d8c8c281d&ids=52f78dcf4293916d8c8c2820&ids=52f78dcf4293916d8c8c2821&ids=52f78dcf4293916d8c8c2824');
+		});
+		
+		it('should get task details for user tasks', function() {
+			expect(scope.tasks.length).toBe(4);
+			expect(scope.tasks[3]._id).toBe('task7');
+		});
+	});
+	
+	describe('user driven events', function() {
+		beforeEach(inject(function() {
+			httpMock.whenGET('/api/users/52f78c914293916d8c8c281b').respond(mockApiResponseUser)
+			httpMock.whenGET('/api/tasks?ids=52f78dcf4293916d8c8c281d&ids=52f78dcf4293916d8c8c2820&ids=52f78dcf4293916d8c8c2821&ids=52f78dcf4293916d8c8c2824').respond(mockApiResponseTasks)
+			httpMock.whenDELETE('/api/users/52f78c914293916d8c8c281b/task3').respond(mockApiResponseUserDeletedTask);
+			ctrl = controllerSvc('myTasksController', {$scope: scope});
+			httpMock.flush();
+		}));
+		
+		describe('delete time', function() {			
+			it('should prompt the user to confirm deletion', function() {
+				var confirm_msg;
+				spyOn(window, 'confirm').andCallFake(function(msg) {
+					confirm_msg = msg;
+					return false;
+				});
+				spyOn(scope, 'deleteTask').andCallThrough();
+				scope.deleteTask('task3');
+				expect(confirm_msg).toBe('Are you sure you want to delete this task?');
+			});
+			
+			it('should remove a row from tasks', function() {
+				spyOn(window, 'confirm').andReturn(true);
+				spyOn(scope, 'deleteTask').andCallThrough();
+				scope.deleteTask('task3');
+				httpMock.flush();
+				
+				var arr = new Array();
+				arr.push(mockApiResponseTasks[0]);
+				arr.push(mockApiResponseTasks[2]);
+				arr.push(mockApiResponseTasks[3]);
+				expect(scope.tasks.length).toBe(3);
+				expect(JSON.stringify(scope.tasks)).toEqual(JSON.stringify(arr));
+			});
+			
+			it('should call deleteTask', function() {
+				spyOn(window, 'confirm').andReturn(true);
+				spyOn(scope, 'deleteTask').andCallThrough();
+				scope.deleteTask('task3');
+				
+				expect(scope.deleteTask).toHaveBeenCalled();
+			});
+			
+			
+			it('should DELETE task from users taskBank', function() {
+				spyOn(window, 'confirm').andReturn(true);
+				spyOn(scope, 'deleteTask').andCallThrough();
+				
+				httpMock.expectDELETE('/api/users/52f78c914293916d8c8c281b/task3');
+				ctrl = controllerSvc('myTasksController', {$scope: scope});
+				scope.deleteTask('task3');
+				httpMock.flush();
+			});
+		});
+	});
+});
